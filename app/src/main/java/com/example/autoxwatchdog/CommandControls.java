2128f6245564bf0389573a7554012189480cb2fb
Cavan Biggs <cbiggs7635@conestogac.on.ca>
2020-03-21 00:06:29 -0400

    Finished basic app functionality

--- app/src/main/java/com/example/autoxwatchdog/CommandControls.java
+++ app/src/main/java/com/example/autoxwatchdog/CommandControls.java
@@ -2,6 +2,9 @@ package com.example.autoxwatchdog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class CommandControls extends AppCompatActivity {
 
@@ -9,5 +12,35 @@ public class CommandControls extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_command_controls);
+
     }
+    // Display a toast message to the user
+    public void displayToast(String message){
+        Toast.makeText(getApplicationContext(),message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    // Handle command buttons
+    public void handleCommand(View v){
+            switch (v.getId()){
+                case R.id.get_front:
+                    //Call method to capture front image
+                    displayToast("Capture front image");
+                    break;
+                case R.id.get_left:
+                    //Call method to capture left image
+                    displayToast("Capture left image");
+                    break;
+                case R.id.get_right:
+                    //Call method to capture right image
+                    displayToast("Capture right image");
+                    break;
+                case R.id.get_rear:
+                    //Call method to capture rear image
+                    displayToast("Capture rear image");
+                    break;
+            }
+        }
+
+
 }
